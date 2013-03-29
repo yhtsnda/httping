67fcdf5b63aaca2c91ca47e1985d67b4e158db5f
folkert <folkert@1aee6d92-366c-48ac-9162-27657959c21e>
2013-03-29 18:19:21 +0000

    fix for buffer underflow when ctrl+c is pressed

--- http.c
+++ http.c
@@ -1,5 +1,6 @@
 /* Released under GPLv2 with exception for the OpenSSL library. See license.txt */
 
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -27,7 +28,7 @@ int get_HTTP_headers(int socket_h, SSL *ssl_h, char **headers, int *overflow, in
 	for(;;)
 	{
 		int rrc;
-		int now_n = (len - len_in) - 1;
+		int now_n = len - len_in;
 
 #ifndef NO_SSL
 		if (ssl_h)
@@ -35,24 +36,29 @@ int get_HTTP_headers(int socket_h, SSL *ssl_h, char **headers, int *overflow, in
 		else
 #endif
 			rrc = read_to(socket_h, &buffer[len_in], now_n, timeout);
+
 		if (rrc == 0 || rrc == RC_SHORTREAD)	/* socket closed before request was read? */
 		{
 			rc = RC_SHORTREAD;
 			break;
 		}
-		else if (rrc == RC_TIMEOUT)		/* timeout */
+		else if (rrc < 0)
 		{
 			free(buffer);
-			return RC_TIMEOUT;
+			return rrc;
 		}
 
 		len_in += rrc;
 
+		assert(len_in >= 0);
+		assert(len_in < len);
+
 		buffer[len_in] = 0x00;
+
 		if (strstr(buffer, "\r\n\r\n") != NULL)
 			break;
 
-		if (len_in == (len - 1))
+		if (len_in >= len)
 		{
 			len <<= 1;
 			buffer = (char *)realloc(buffer, len + 1);
